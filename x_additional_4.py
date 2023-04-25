70c0b9404ccdebfde5959499ee402d289103b075
Shalkar <sh-shalkar-sh@mail.ru>
2023-04-25 16:15:00 +0600

    x_additional_4 Убрал скобки!

--- x_additional_4.py
+++ x_additional_4.py
@@ -3,7 +3,7 @@ m = int(input("Введите m размер шоколадки: "))
 
 k = int(input("Введите k долек, которое хотите отломить: "))
 
-if k < m * n and (k % m == 0 or k % n == 0):
+if k < m * n and k % m == 0 or k % n == 0:
     print('Да')
 else:
     print('Нет')
